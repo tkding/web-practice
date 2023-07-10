4439eb4f7b14bcdfd1909b53a838875f5939e2e9
tkding <tkding@ucsd.edu>
2023-07-10 10:47:45 -0400

    fix /wiki/TITLE

--- project 1/wiki/encyclopedia/urls.py
+++ project 1/wiki/encyclopedia/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.entry, name="title"),
+    path("wiki/<str:name>", views.entry, name="title"),
     path('search/', views.search_results, name="search_results"),
     path('create/', views.create_page, name='create_page'),
     path('edit/<str:name>', views.edit_page, name='edit_page'),
---
+++ project 1/wiki/entries/Beef soup.md
@@ -0,0 +1,13 @@
+2lb beef
+3L water
+1tbs salt
+1tbs pepper
+1tbs msg
+1tbs chicken powder
+1tbs nutmeg powder
+3 onions
+3 celery
+3 carrots
+3 green onions
+2 Ginger root
+2 tomatoes
