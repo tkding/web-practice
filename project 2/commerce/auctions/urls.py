a76e7dbca8034a0c9556ff70fdb07b3e76ab3886
tkding <tkding@ucsd.edu>
2023-07-18 18:23:52 -0400

    Done project2

--- project 2/commerce/auctions/urls.py
+++ project 2/commerce/auctions/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create_listing", views.create_listing, name="create_listing"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("categories", views.category_list, name="category_list"),
+    path("categories/<int:category_id>/", views.category_detail, name="category_detail"),
+    path("<str:listing_id>", views.listing, name="listing"),
 ]
